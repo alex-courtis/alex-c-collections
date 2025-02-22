57e1876ce41264b1f4b780f3ed20eac00add33f0
Alexander Courtis <alex@courtis.org>
2025-02-22 12:35:47 +1100

    add itable_str, oset_str, slist_str

--- tst/tst-itable.c
+++ tst/tst-itable.c
@@ -4,6 +4,8 @@
 #include <cmocka.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "slist.h"
 
@@ -50,6 +52,38 @@ void itable_keys_slist__many(void **state) {
 	itable_free_vals(tab, NULL);
 }
 
+void itable_str__null(void **state) {
+	assert_nul(itable_str(NULL));
+}
+
+void itable_str__empty(void **state) {
+	const struct ITable *tab = itable_init(3, 5);
+
+	char *str = itable_str(tab);
+	assert_str_equal(str, "");
+
+	free(str);
+	itable_free_vals(tab, NULL);
+}
+
+void itable_str__string_vals(void **state) {
+	const struct ITable *tab = itable_init(3, 5);
+
+	itable_put(tab, 10, strdup("1"));
+	itable_put(tab, 20, NULL);
+	itable_put(tab, 30, strdup("3"));
+
+	char *str = itable_str(tab);
+	assert_str_equal(str,
+			"10 = 1\n"
+			"20 = (null)\n"
+			"30 = 3"
+			);
+
+	free(str);
+	itable_free_vals(tab, NULL);
+}
+
 int main(void) {
 	const struct CMUnitTest tests[] = {
 
@@ -57,6 +91,10 @@ int main(void) {
 
 		TEST(itable_keys_slist__empty),
 		TEST(itable_keys_slist__many),
+
+		TEST(itable_str__null),
+		TEST(itable_str__empty),
+		TEST(itable_str__string_vals),
 	};
 
 	return RUN(tests);
