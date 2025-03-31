d3e9ae7bd862cd6e5f27d0de84539c14cd237ee3
Alexander Courtis <alex@courtis.org>
2025-03-31 11:11:00 +1100

    slist: simplify xor tests

--- tst/tst-slist.c
+++ tst/tst-slist.c
@@ -11,12 +11,6 @@
 
 #include "slist.h"
 
-struct State {
-	struct SList *list1;
-	struct SList *list2;
-	struct SList *expected;
-};
-
 int before_all(void **state) {
 	return 0;
 }
@@ -26,24 +20,10 @@ int after_all(void **state) {
 }
 
 int before_each(void **state) {
-	struct State *s = calloc(1, sizeof(struct State));
-
-	s->list1 = NULL;
-	s->list2 = NULL;
-	s->expected = NULL;
-
-	*state = s;
 	return 0;
 }
 
 int after_each(void **state) {
-	struct State *s = *state;
-
-	slist_free_vals(&s->list1, NULL);
-	slist_free_vals(&s->list2, NULL);
-	slist_free_vals(&s->expected, NULL);
-
-	free(s);
 	return 0;
 }
 
@@ -656,74 +636,99 @@ void slist_str__string_vals(void **state) {
 }
 
 void slist_xor_free__empty_lists(void **state) {
-	struct State *s = *state;
+	struct SList *list1 = NULL;
+	struct SList *list2 = NULL;
 
-	slist_xor_free(&s->list1, s->list2, fn_comp_equals_strcmp, NULL, fn_clone_strdup);
+	slist_xor_free(&list1, list2, fn_comp_equals_strcmp, NULL, fn_clone_strdup);
 
-	assert_int_equal(slist_length(s->list1), 0);
+	assert_int_equal(slist_length(list1), 0);
 }
 
 void slist_xor_free__first_list_empty(void **state) {
-	struct State *s = *state;
+	struct SList *list1 = NULL;
+	struct SList *list2 = NULL;
+	struct SList *expected = NULL;
 
-	slist_append(&s->list2, strdup("item1"));
-	slist_append(&s->list2, strdup("item2"));
+	slist_append(&list2, strdup("item1"));
+	slist_append(&list2, strdup("item2"));
 
-	slist_append(&s->expected, strdup("item1"));
-	slist_append(&s->expected, strdup("item2"));
+	slist_append(&expected, strdup("item1"));
+	slist_append(&expected, strdup("item2"));
 
-	slist_xor_free(&s->list1, s->list2, fn_comp_equals_strcmp, NULL, fn_clone_strdup);
+	slist_xor_free(&list1, list2, fn_comp_equals_strcmp, NULL, fn_clone_strdup);
 
-	assert_true(slist_equal(s->list1, s->expected, fn_comp_equals_strcmp));
+	assert_true(slist_equal(list1, expected, fn_comp_equals_strcmp));
+
+	slist_free_vals(&list1, NULL);
+	slist_free_vals(&list2, NULL);
+	slist_free_vals(&expected, NULL);
 }
 
 void slist_xor_free__second_list_empty(void **state) {
-	struct State *s = *state;
+	struct SList *list1 = NULL;
+	struct SList *list2 = NULL;
+	struct SList *expected = NULL;
 
-	slist_append(&s->list1, strdup("item1"));
-	slist_append(&s->list1, strdup("item2"));
+	slist_append(&list1, strdup("item1"));
+	slist_append(&list1, strdup("item2"));
 
-	slist_append(&s->expected, strdup("item1"));
-	slist_append(&s->expected, strdup("item2"));
+	slist_append(&expected, strdup("item1"));
+	slist_append(&expected, strdup("item2"));
 
-	slist_xor_free(&s->list1, s->list2, fn_comp_equals_strcmp, NULL, fn_clone_strdup);
+	slist_xor_free(&list1, list2, fn_comp_equals_strcmp, NULL, fn_clone_strdup);
 
-	assert_true(slist_equal(s->list1, s->expected, fn_comp_equals_strcmp));
+	assert_true(slist_equal(list1, expected, fn_comp_equals_strcmp));
+
+	slist_free_vals(&list1, NULL);
+	slist_free_vals(&list2, NULL);
+	slist_free_vals(&expected, NULL);
 }
 
 void slist_xor_free__toggle_items(void **state) {
-	struct State *s = *state;
+	struct SList *list1 = NULL;
+	struct SList *list2 = NULL;
+	struct SList *expected = NULL;
 
-	slist_append(&s->list1, strdup("item1"));
-	slist_append(&s->list1, strdup("item2"));
-	slist_append(&s->list1, strdup("item3"));
+	slist_append(&list1, strdup("item1"));
+	slist_append(&list1, strdup("item2"));
+	slist_append(&list1, strdup("item3"));
 
-	slist_append(&s->list2, strdup("item2"));
-	slist_append(&s->list2, strdup("item4"));
+	slist_append(&list2, strdup("item2"));
+	slist_append(&list2, strdup("item4"));
 
-	slist_append(&s->expected, strdup("item1"));
-	slist_append(&s->expected, strdup("item3"));
-	slist_append(&s->expected, strdup("item4"));
+	slist_append(&expected, strdup("item1"));
+	slist_append(&expected, strdup("item3"));
+	slist_append(&expected, strdup("item4"));
 
-	slist_xor_free(&s->list1, s->list2, fn_comp_equals_strcmp, NULL, fn_clone_strdup);
+	slist_xor_free(&list1, list2, fn_comp_equals_strcmp, NULL, fn_clone_strdup);
 
-	assert_true(slist_equal(s->list1, s->expected, fn_comp_equals_strcmp));
+	assert_true(slist_equal(list1, expected, fn_comp_equals_strcmp));
+
+	slist_free_vals(&list1, NULL);
+	slist_free_vals(&list2, NULL);
+	slist_free_vals(&expected, NULL);
 }
 
 void slist_xor_free__duplicate_items(void **state) {
-	struct State *s = *state;
+	struct SList *list1 = NULL;
+	struct SList *list2 = NULL;
+	struct SList *expected = NULL;
 
-	slist_append(&s->list1, strdup("item1"));
-	slist_append(&s->list1, strdup("item1")); // duplicate
-	slist_append(&s->list1, strdup("item2"));
+	slist_append(&list1, strdup("item1"));
+	slist_append(&list1, strdup("item1")); // duplicate
+	slist_append(&list1, strdup("item2"));
 
-	slist_append(&s->list2, strdup("item1"));
+	slist_append(&list2, strdup("item1"));
 
-	slist_append(&s->expected, strdup("item2"));
+	slist_append(&expected, strdup("item2"));
 
-	slist_xor_free(&s->list1, s->list2, fn_comp_equals_strcmp, NULL, fn_clone_strdup);
+	slist_xor_free(&list1, list2, fn_comp_equals_strcmp, NULL, fn_clone_strdup);
 
-	assert_true(slist_equal(s->list1, s->expected, fn_comp_equals_strcmp));
+	assert_true(slist_equal(list1, expected, fn_comp_equals_strcmp));
+
+	slist_free_vals(&list1, NULL);
+	slist_free_vals(&list2, NULL);
+	slist_free_vals(&expected, NULL);
 }
 
 
