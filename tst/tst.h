7d44a99a6d04c0e04d2402ba72ae1ab617a8bd06
Alexander Courtis <alex@courtis.org>
2026-06-16 12:15:43 +1000

    test: before/after test definition macro variants

--- tst/tst.h
+++ tst/tst.h
@@ -8,6 +8,13 @@
 // test definition
 //
 #define TEST(t) cmocka_unit_test(t)
+#define TEST_A(t) cmocka_unit_test_setup_teardown(t, NULL, after_each)
+#define TEST_B(t) cmocka_unit_test_setup_teardown(t, before_each, NULL)
+#define TEST_BA(t) cmocka_unit_test_setup_teardown(t, before_each, after_each)
+
 #define RUN(t) cmocka_run_group_tests(t, NULL, NULL)
+#define RUN_A(t) cmocka_run_group_tests(t, NULL, after_all)
+#define RUN_B(t) cmocka_run_group_tests(t, before_all, NULL)
+#define RUN_BA(t) cmocka_run_group_tests(t, before_all, after_all)
 
 #endif // TST_H
